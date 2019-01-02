08a40f320262ee688e2711c87a6f3f7f7bda9b71
toydev <toy@ownway.info>
2019-01-02 23:46:13 +0900

    コメントを入れるなど、わかりやすさを改善した。

--- Notification/MainWindowViewModel.cs
+++ Notification/MainWindowViewModel.cs
@@ -10,6 +10,8 @@ namespace Notification
 {
     class MainWindowViewModel
     {
+        public const int COUNTER_KEY = 1;
+
         public RealmConfiguration Config { get; }
         public Counter Counter { get; }
         public ICommand CountUpCommand { get; }
@@ -21,10 +23,10 @@ namespace Notification
 
             var realm = Realm.GetInstance(Config);
 
-            Counter = realm.Find<Counter>(1);
+            Counter = realm.Find<Counter>(COUNTER_KEY);
             if (Counter == null)
             {
-                Counter = new Counter { CounterKey = 1, Count = 0, };
+                Counter = new Counter { CounterKey = COUNTER_KEY, Count = 0, };
                 realm.Write(() =>
                 {
                     realm.Add(Counter);
@@ -40,7 +42,10 @@ namespace Notification
 
             realm.Write(() =>
             {
-                var target = realm.Find<Counter>(1);
+                // Realm.Find で取得しなおしたオブジェクト経由で変更しても変更通知は伝わる。
+                var target = realm.Find<Counter>(COUNTER_KEY);
+
+                // INotifyPropertyChanged による変更通知で画面の表示が自動で変わる。
                 target.Count++;
             });
         }
