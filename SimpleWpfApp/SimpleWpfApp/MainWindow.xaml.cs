12002e4f1bd0de056fb53c53b97799384af58506
toydev <toy@ownway.info>
2019-01-02 22:47:04 +0900

    SimpleWpfApp を追加した。

---
+++ SimpleWpfApp/SimpleWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace SimpleWpfApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ SimpleWpfApp/SimpleWpfApp/Model/Counter.cs
@@ -0,0 +1,11 @@
+﻿using Realms;
+
+namespace SimpleWpfApp.Model
+{
+    class Counter : RealmObject
+    {
+        [PrimaryKey]
+        public int CounterKey { get; set; }
+        public int Count { get; set; }
+    }
+}
